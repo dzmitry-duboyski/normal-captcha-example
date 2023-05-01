1d2822afd1047dbe21d6729b931c6bf767ea9be9
Dima Dub <dzmitry.duboyski@gmail.com>
2023-05-01 10:26:49 +0400

    refactor

--- index.js
+++ index.js
@@ -1,67 +1,73 @@
-const fs = require('fs');
-const puppeteer = require('puppeteer');
-const chalk = require('chalk');
-const Captcha = require('2captcha');
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const chalk = require("chalk");
+const Captcha = require("2captcha");
 const APIKEY = process.env.APIKEY;
 const solver = new Captcha.Solver(APIKEY);
 
 (async () => {
   const puppeteerSettings = {
-    headless: true
-  }
+    headless: true,
+  };
 
-  const browser = await puppeteer.launch(puppeteerSettings)
+  const browser = await puppeteer.launch(puppeteerSettings);
   const page = await browser.newPage();
-  await page.goto('https://2captcha.com/demo/normal')
+  await page.goto("https://2captcha.com/demo/normal");
 
-  await page.waitForSelector('img[alt="normal captcha example"]')
-  const element = await page.$('img[alt="normal captcha example"]')
-  await element.screenshot({path: './captchas/image.png'})
-  
+  await page.waitForSelector('img[alt="normal captcha example"]');
+  const element = await page.$('img[alt="normal captcha example"]');
+  // save captcha
+  await element.screenshot({ path: "./captchas/image.png" });
+
   const getCaptchaAnswer = async () => {
     try {
-      const res = await solver.imageCaptcha(fs.readFileSync('./captchas/image.png', 'base64'))
-      return res.data
+      //send captcha
+      const res = await solver.imageCaptcha(
+        fs.readFileSync("./captchas/image.png", "base64")
+      );
+      return res.data;
     } catch (err) {
       console.log(err);
     }
-  }
+  };
 
-  const captchaAnswer = await getCaptchaAnswer()
-  console.log('captchaAnswer:' + captchaAnswer)
+  const captchaAnswer = await getCaptchaAnswer();
+  console.log("captchaAnswer:" + captchaAnswer);
 
-  await page.type('#simple-captcha-field', captchaAnswer, {delay: 500})
+  //set captcha answer
+  await page.type("#simple-captcha-field", captchaAnswer, { delay: 500 });
   await page.evaluate(() => {
-    document.querySelector('button[type=submit]').click()
-  })
+    //click on 'check'
+    document.querySelector("button[type=submit]").click();
+  });
 
   const getResult = await page.evaluate(() => {
-      let result
+    let result;
 
-      const errorElem = document.querySelector('._1Or-n9RKBk1X_Bc_vZYSf4')
-      if(!errorElem){
-        // console.log('element not found')
-      } else {
-        console.log('errorMsg: '+ errorElem.innerText)
-        result = 'Incorrect CAPTCHA answer, please try again.'
-        return result
-      }
+    const errorElem = document.querySelector("._1Or-n9RKBk1X_Bc_vZYSf4");
+    if (!errorElem) {
+      // console.log('element not found')
+    } else {
+      console.log("errorMsg: " + errorElem.innerText);
+      result = "Incorrect CAPTCHA answer, please try again.";
+      return result;
+    }
 
-      const successElem = document.querySelector('.j4U8b8WW7BD_DOSsopoys')
-      if(!successElem){
-        // console.log('element not found')
-      } else {
-        result = 'Captcha is passed successfully!'
-        return result
-      }
-  })
+    const successElem = document.querySelector(".j4U8b8WW7BD_DOSsopoys");
+    if (!successElem) {
+      // console.log('element not found')
+    } else {
+      result = "Captcha is passed successfully!";
+      return result;
+    }
+  });
 
-  const isSuccess = getResult == 'Captcha is passed successfully!'
-  if(isSuccess) {
-    console.log(chalk.bgGreen(getResult))
+  const isSuccess = getResult == "Captcha is passed successfully!";
+  if (isSuccess) {
+    console.log(chalk.bgGreen(getResult));
   } else {
-    console.log(chalk.bgRed(getResult))
+    console.log(chalk.bgRed(getResult));
   }
 
-  browser.close()
+  browser.close();
 })();
